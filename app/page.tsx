69d310ae6f1ebef55b8c55aa9e59f0cf1c3bc7ea
mohdshahzil <theshahzil@gmail.com>
2024-03-26 04:35:14 +0530

    sidebar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
 import AppbarNoLogin from "@/components/Appbar/AppbarNoLogin";
 import Heading from "@/components/Heading";
+// import { MoreVertical, Activity, Bell } from "lucide-react";
+// import Sidebar, { SidebarItem } from "@/components/Sidebar/Sidebar";
 const page = () => {
   return (
     <div className=" ">
-      {" "}
       <AppbarNoLogin></AppbarNoLogin>
       <Heading></Heading>
+      {/* <Sidebar>
+        <SidebarItem icon={<Activity />} text="Dashboard" active />
+        <SidebarItem icon={<Bell />} text="Notifications" alert />
+      </Sidebar> */}
     </div>
   );
 };
