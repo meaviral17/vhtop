12631747b48b45d9f4f2199c3c0b0bd197b36201
mohdshahzil <theshahzil@gmail.com>
2024-03-26 03:27:50 +0530

    /content route added

---
+++ app/content/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      {/* make spotlight a component and then import it here ,add dummy data and follow the vtop ui  */}
+    </div>
+  )
+}
+
+export default page
