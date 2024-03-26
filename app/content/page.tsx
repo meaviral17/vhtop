f1c0b1e12400ff4dd5012d85afb8481a65daaeb8
mohdshahzil <theshahzil@gmail.com>
2024-03-27 05:24:43 +0530

    sidebar added

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,10 +1,12 @@
 import Spotlight from "@/components/Spotlight/Spotlight";
 import React from "react";
 import AppbarLogin from "@/components/Appbar/AppbarLogin";
+import Sidebar from "@/components/Sidebar/Sidebar";
 const page = () => {
   return (
     <div>
       <AppbarLogin></AppbarLogin>
+      <Sidebar></Sidebar>
       <Spotlight />
     </div>
   );
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="h-full w-8  flex flex-col">
-      <div className="h-14"></div>
+    <div className="-z-10 items-center h-auto w-8 absolute left-0 flex flex-col">
+      {/* <div className="h-14"></div> */}
       <div>1</div>
       <div>2</div>
       <div>3</div>
