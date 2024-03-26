dc820afff70e389ae500942905b7bac82a596f61
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-03-26 13:06:13 +0530

    Spotlight-1

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,9 +1,11 @@
+import Spotlight from '@/components/Spotlight/Spotlight'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       {/* make spotlight a component and then import it here ,add dummy data and follow the vtop ui  */}
+      <Spotlight/>
     </div>
   )
 }
