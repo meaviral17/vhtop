43e443d04d17084d304769b66d9aafb786dd3419
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-03-27 23:36:51 +0530

    Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import AppbarNoLogin from "@/components/Appbar/AppbarNoLogin";
 import Heading from "@/components/Heading";
+import Card from "@/components/Card/Card";
+import dummyData from "./content/dummyData";
 // import { MoreVertical, Activity, Bell } from "lucide-react";
 // import Sidebar, { SidebarItem } from "@/components/Sidebar/Sidebar";
 const page = () => {
@@ -12,6 +14,19 @@ const page = () => {
         <SidebarItem icon={<Activity />} text="Dashboard" active />
         <SidebarItem icon={<Bell />} text="Notifications" alert />
       </Sidebar> */}
+      <div className="sm:flex  sm:m-4 m-3  ">
+
+        <Card
+          title={dummyData[0].title}
+          content={dummyData[0].content}
+          icon={dummyData[0].icon}
+        />
+        <Card
+          title={dummyData[1].title}
+          content={dummyData[1].content}
+          icon={dummyData[1].icon}
+        />
+      </div>
     </div>
   );
 };
