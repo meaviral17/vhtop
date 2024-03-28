46ee0e4c32d393b7bd892926429a21a57d86140a
mohdshahzil <theshahzil@gmail.com>
2024-03-28 07:03:10 +0530

    multiple features routes

---
+++ app/content/food-park/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppbarLogin from "@/components/Appbar/AppbarLogin";
+import Sidebar from "@/components/Sidebar/Sidebar";
+const page = () => {
+  return (
+    <div>
+      <AppbarLogin></AppbarLogin>
+      <Sidebar></Sidebar>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/content/vshare/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppbarLogin from "@/components/Appbar/AppbarLogin";
+import Sidebar from "@/components/Sidebar/Sidebar";
+const page = () => {
+  return (
+    <div>
+      <AppbarLogin></AppbarLogin>
+      <Sidebar></Sidebar>
+      i am from v share
+    </div>
+  );
+};
+
+export default page;
