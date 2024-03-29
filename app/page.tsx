e67b968e73bb1fde0dd9d09fb9ee59b0892b9844
mohdshahzil <theshahzil@gmail.com>
2024-03-30 04:59:09 +0530

    hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Heading from "@/components/Heading";
 import Card from "@/components/Card/Card";
 import dummyData from "./content/dummyData";
 import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
 const page = () => {
   return (
     <div className=" ">
       <AppbarNoLogin></AppbarNoLogin>
       <Heading></Heading>
+      <HeroSection></HeroSection>
       <div className="sm:flex  sm:m-4 m-3  ">
         <Card
           title={dummyData[0].title}
