78165b9de42914db7ddbdcb1d051dd7a8019ec61
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-03-30 20:36:28 +0530

    Update Loginform.tsx

--- components/LoginForm/Loginform.tsx
+++ components/LoginForm/Loginform.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Login() {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden ">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl ">
         <h1 className=" font-bold text-center text-primary text-4xl ">VHTOP Login</h1>
         <form className="mt-6">
           <div className="mb-4">
@@ -30,14 +30,8 @@ export default function Login() {
               className="block w-full px-4 py-2 mt-2 text-primary bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          <Link
-            href="/forget"
-            className="text-xs text-blue-600 hover:underline"
-          >
-            Forget Password?
-          </Link>
           <div className="mt-2">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-primary rounded-md hover:bg-primary-600 focus:outline-none focus:bg-gray-600">
+          <button className="w-full px-4 py-2 tracking-wide text-primary transition-colors duration-200 transform bg-primary text-white rounded-md hover:bg-primary hover:text-white focus:outline-none focus:bg-primary focus:text-white">
               Login
             </button>
           </div>
