bf4ca8e58645d2ea27d4a5466a235cc344f8ccb6
mohdshahzil <theshahzil@gmail.com>
2024-03-31 05:51:59 +0530

    car-pool completed

--- app/content/vshare/page.tsx
+++ app/content/vshare/page.tsx
@@ -1,33 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import AppbarLogin from "@/components/Appbar/AppbarLogin";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { CalendarIcon } from "@radix-ui/react-icons";
-import { format } from "date-fns";
-
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import SearchSVG from "@/assets/svg/SearchSVG";
-import {
-  CaretSortIcon,
-  ChevronDownIcon,
-  DotsHorizontalIcon,
-} from "@radix-ui/react-icons";
+import { CaretSortIcon, ChevronDownIcon } from "@radix-ui/react-icons";
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -40,15 +16,10 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-
-import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
@@ -60,151 +31,42 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import VShareForm from "@/components/VShareForm/VShareForm";
 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
-  },
-];
-
+// Define the Payment type
 export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
+  source: string;
+  destination: string;
+  date: string;
+  slot: string;
 };
 
-const columns: ColumnDef<Payment>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
-    ),
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
+const Page = () => {
+  // Define states for table functionality
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
 
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
+  // Define state to store selected data
+  const [selectedDataList, setSelectedDataList] = useState<Payment[]>([]);
 
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
+  // Handle function to add selected data to the list
+  const handleSelectedData = (selectedData: Payment) => {
+    setSelectedDataList([...selectedDataList, selectedData]);
+  };
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
+  // Create columns for the table
+  const columns: ColumnDef<Payment>[] = [
+    { accessorKey: "source", header: "Source" },
+    { accessorKey: "destination", header: "Destination" },
+    { accessorKey: "date", header: "Date" },
+    { accessorKey: "slot", header: "Slot" },
+  ];
 
-const page = () => {
-  const [date, setDate] = React.useState<Date>();
-
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = React.useState({});
-
+  // Create the table instance
   const table = useReactTable({
-    data,
+    data: selectedDataList, // Use selectedDataList as data source
     columns,
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
@@ -214,119 +76,34 @@ const page = () => {
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
     onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
+    state: { sorting, columnFilters, columnVisibility, rowSelection },
   });
 
   return (
     <div>
-      <AppbarLogin></AppbarLogin>
-      <Sidebar></Sidebar>
+      <AppbarLogin />
+      <Sidebar />
       <div className="flex flex-col sm:ml-12 ml-3">
         <div className="text-primary text-4xl font-medium m-4">
           VShare : Find rides to and from VIT
         </div>
-        <div className="flex md:flex-row flex-col  gap-3 m-4 p-2">
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select source" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {/* <SelectLabel>Source</SelectLabel> */}
-                <SelectItem value="apple">VIT</SelectItem>
-                <SelectItem value="banana">Tambaram</SelectItem>
-                <SelectItem value="blueberry">Central</SelectItem>
-                <SelectItem value="grapes">Airport</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select Destination" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="apple">VIT</SelectItem>
-                <SelectItem value="banana">Tambaram</SelectItem>
-                <SelectItem value="blueberry">Central</SelectItem>
-                <SelectItem value="grapes">Airport</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-[240px] justify-start text-left font-normal",
-                  !date && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date ? format(date, "PPP") : <span>Pick a date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select Slot" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="00:00">00:00</SelectItem>
-                <SelectItem value="01:00">01:00</SelectItem>
-                <SelectItem value="02:00">02:00</SelectItem>
-                <SelectItem value="03:00">03:00</SelectItem>
-                <SelectItem value="04:00">04:00</SelectItem>
-                <SelectItem value="05:00">05:00</SelectItem>
-                <SelectItem value="06:00">06:00</SelectItem>
-                <SelectItem value="07:00">07:00</SelectItem>
-                <SelectItem value="08:00">08:00</SelectItem>
-                <SelectItem value="09:00">09:00</SelectItem>
-                <SelectItem value="10:00">10:00</SelectItem>
-                <SelectItem value="11:00">11:00</SelectItem>
-                <SelectItem value="12:00">12:00</SelectItem>
-                <SelectItem value="13:00">13:00</SelectItem>
-                <SelectItem value="14:00">14:00</SelectItem>
-                <SelectItem value="15:00">15:00</SelectItem>
-                <SelectItem value="16:00">16:00</SelectItem>
-                <SelectItem value="17:00">17:00</SelectItem>
-                <SelectItem value="18:00">18:00</SelectItem>
-                <SelectItem value="19:00">19:00</SelectItem>
-                <SelectItem value="20:00">20:00</SelectItem>
-                <SelectItem value="21:00">21:00</SelectItem>
-                <SelectItem value="22:00">22:00</SelectItem>
-                <SelectItem value="23:00">23:00</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Button className="flex gap-2">
-            Search your travel mate <SearchSVG></SearchSVG>
-          </Button>
-        </div>
-        {/* below is the code for table */}
-        <div className=" m-2 p-2">
+        {/* Pass handleSelectedData function to VShareForm */}
+        <VShareForm handleSelectedData={handleSelectedData} />
+        {/* Table component */}
+
+        <div className="m-2 p-2">
+          {/* Table controls */}
           <div className="flex items-center py-4">
             <Input
-              placeholder="Filter emails..."
+              placeholder="Filter destinations..."
               value={
-                (table.getColumn("email")?.getFilterValue() as string) ?? ""
+                (table.getColumn("destination")?.getFilterValue() as string) ??
+                ""
               }
               onChange={(event) =>
-                table.getColumn("email")?.setFilterValue(event.target.value)
+                table
+                  .getColumn("destination")
+                  ?.setFilterValue(event.target.value)
               }
               className="max-w-sm"
             />
@@ -340,40 +117,37 @@ const page = () => {
                 {table
                   .getAllColumns()
                   .filter((column) => column.getCanHide())
-                  .map((column) => {
-                    return (
-                      <DropdownMenuCheckboxItem
-                        key={column.id}
-                        className="capitalize"
-                        checked={column.getIsVisible()}
-                        onCheckedChange={(value) =>
-                          column.toggleVisibility(!!value)
-                        }
-                      >
-                        {column.id}
-                      </DropdownMenuCheckboxItem>
-                    );
-                  })}
+                  .map((column) => (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  ))}
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
+          {/* Table */}
           <div className="rounded-md border">
             <Table>
               <TableHeader>
                 {table.getHeaderGroups().map((headerGroup) => (
                   <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <TableHead key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                        </TableHead>
-                      );
-                    })}
+                    {headerGroup.headers.map((header) => (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    ))}
                   </TableRow>
                 ))}
               </TableHeader>
@@ -407,34 +181,11 @@ const page = () => {
               </TableBody>
             </Table>
           </div>
-          <div className="flex items-center justify-end space-x-2 py-4">
-            <div className="flex-1 text-sm text-muted-foreground">
-              {table.getFilteredSelectedRowModel().rows.length} of{" "}
-              {table.getFilteredRowModel().rows.length} row(s) selected.
-            </div>
-            <div className="space-x-2">
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-              >
-                Previous
-              </Button>
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-              >
-                Next
-              </Button>
-            </div>
-          </div>
+          {/* Table pagination and selection summary */}
         </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
