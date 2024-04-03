fb008e2da3f9dca2a9076a25b1b9e197bc044d6e
mohdshahzil <theshahzil@gmail.com>
2024-04-03 21:14:48 +0530

    data added

--- app/content/night-mess/NightVegData.ts
+++ app/content/night-mess/NightVegData.ts
@@ -5,5 +5,6 @@ export const nightVegData = [
   { title: "Paneer Masala Dosa", price: "Rs. 60/-", image: paneerbuttermasaladosa },
   { title: "Some Other Food", price: "Rs. 50/-", image: paneerbuttermasaladosa },
   { title: "Surprise ;)", price: "Rs. 6000/-", image: paneerbuttermasaladosa },
+  { title: "Surprise ;)", price: "Rs. 6000/-", image: paneerbuttermasaladosa },
   // Add more items here
 ];
