3391bae0a2c1c29105a29bff8888dd3170b8c2d3
mohdshahzil <theshahzil@gmail.com>
2024-04-07 23:04:42 +0530

    tooltip trigger added

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -3,55 +3,74 @@ import calling from "@/assets/calling.png";
 import clothes from "@/assets/clothes.png";
 import chatbot from "@/assets/chatbot.png";
 import studyHub from "@/assets/studyHub.png";
-import vshare from "@/assets/vshare.png";
 import food from "@/assets/food.png";
 import sports from "@/assets/sports.png";
-
+import carsharing from "@/assets/car-sharing.png";
 import complaints from "@/assets/complaints.png";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 const Sidebar = () => {
   return (
-    <div className=" items-center h-auto w-8 absolute left-0 flex flex-col p-2 gap-6">
+    <div className=" items-center h-auto w-10 absolute left-0 flex flex-col p-2 gap-6">
       <Link href="/content/complaints">
-        <div>
-          <Image src={complaints} height={25} width={25} alt="contacts"></Image>
-        </div>
+        <TooltipProvider delayDuration={10}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <Image
+                  src={complaints}
+                  height={30}
+                  width={30}
+                  alt="contacts"
+                ></Image>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Complaints</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </Link>
       <Link href="/content/contacts">
         <div>
-          <Image src={calling} height={25} width={25} alt="contacts"></Image>
+          <Image src={calling} height={30} width={30} alt="contacts"></Image>
         </div>
       </Link>
 
       <Link href="/content/food-park">
         <div>
-          <Image src={food} height={25} width={25} alt="Mess"></Image>
+          <Image src={food} height={30} width={30} alt="Mess"></Image>
         </div>
       </Link>
       <Link href="/content/games">
         <div>
-          <Image src={sports} height={25} width={25} alt="laundary"></Image>
+          <Image src={sports} height={30} width={30} alt="laundary"></Image>
         </div>
       </Link>
       <Link href="/content/night-mess">
         <div>
-          <Image src={sports} height={25} width={25} alt="games"></Image>
+          <Image src={sports} height={30} width={30} alt="games"></Image>
         </div>
       </Link>
       <Link href="/content/study-hub">
         <div>
-          <Image src={studyHub} height={25} width={25} alt="contacts"></Image>
+          <Image src={studyHub} height={30} width={30} alt="contacts"></Image>
         </div>
       </Link>
       <Link href="/content/vshare">
         <div>
-          <Image src={vshare} height={25} width={25} alt="contacts"></Image>
+          <Image src={carsharing} height={30} width={30} alt="contacts"></Image>
         </div>
       </Link>
       <Link href="/content/v-chat">
         <div>
-          <Image src={chatbot} height={25} width={25} alt="contacts"></Image>
+          <Image src={chatbot} height={30} width={30} alt="vchat"></Image>
         </div>
       </Link>
     </div>
