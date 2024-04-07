443c330de47b859fb9526ab4589a1d6c8b76b5d3
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-04-08 00:26:30 +0530

    Sidebar popup

--- components/Sidebar/AdminSidebar.tsx
+++ components/Sidebar/AdminSidebar.tsx
@@ -2,51 +2,67 @@ import React from "react";
 import calling from "@/assets/calling.png";
 import clothes from "@/assets/clothes.png";
 import studyHub from "@/assets/studyHub.png";
-import vshare from "@/assets/vshare.png";
+import carsharing from "@/assets/car-sharing.png";
 import food from "@/assets/food.png";
 import sports from "@/assets/sports.png";
 import complaints from "@/assets/complaints.png";
 import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 import Link from "next/link";
+const links = [
+  {
+    href: "/content/complaints",
+    src: complaints,
+    alt: "complaints",
+    tooltip: "Complaints",
+  },
+  {
+    href: "/content/contacts",
+    src: calling,
+    alt: "contacts",
+    tooltip: "Contacts",
+  },
+  { href: "/content/food-park", src: food, alt: "Mess", tooltip: "Mess" },
+  { href: "/content/games", src: sports, alt: "laundary", tooltip: "Laundry" },
+  { href: "/content/night-mess", src: sports, alt: "games", tooltip: "Games" },
+  {
+    href: "/content/study-hub",
+    src: studyHub,
+    alt: "contacts",
+    tooltip: "Study Hub",
+  },
+  {
+    href: "/content/vshare",
+    src: carsharing,
+    alt: "contacts",
+    tooltip: "VShare",
+  },
+];
 const AdminSidebar = () => {
   return (
     <div className=" items-center h-auto w-8 absolute left-0 flex flex-col p-2 gap-6">
-      <Link href="/content/complaints">
-        <div>
-          <Image src={complaints} height={25} width={25} alt="contacts"></Image>
-        </div>
-      </Link>
-      {/* <Link href="/content/contacts">
-        <div>
-          <Image src={calling} height={25} width={25} alt="contacts"></Image>
-        </div>
-      </Link>
-
-      <Link href="/content/food-park">
-        <div>
-          <Image src={food} height={25} width={25} alt="Mess"></Image>
-        </div>
-      </Link>
-      <Link href="/content/games">
-        <div>
-          <Image src={sports} height={25} width={25} alt="laundary"></Image>
-        </div>
-      </Link>
-      <Link href="/content/night-mess">
-        <div>
-          <Image src={sports} height={25} width={25} alt="games"></Image>
-        </div>
-      </Link>
-      <Link href="/content/study-hub">
-        <div>
-          <Image src={studyHub} height={25} width={25} alt="contacts"></Image>
-        </div>
-      </Link>
-      <Link href="/content/vshare">
-        <div>
-          <Image src={vshare} height={25} width={25} alt="contacts"></Image>
-        </div>
-      </Link> */}
+      <TooltipProvider delayDuration={10}>
+        {links.map(({ href, src, alt, tooltip }, index) => (
+          <Link key={index} href={href}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <div>
+                  <Image src={src} height={30} width={30} alt={alt} />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{tooltip}</p>
+              </TooltipContent>
+            </Tooltip>
+          </Link>
+        ))}
+      </TooltipProvider>
     </div>
   );
 };
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import calling from "@/assets/calling.png";
-import clothes from "@/assets/clothes.png";
+
 import chatbot from "@/assets/chatbot.png";
 import studyHub from "@/assets/studyHub.png";
 import food from "@/assets/food.png";
@@ -15,64 +15,56 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+
+const links = [
+  {
+    href: "/content/complaints",
+    src: complaints,
+    alt: "complaints",
+    tooltip: "Complaints",
+  },
+  {
+    href: "/content/contacts",
+    src: calling,
+    alt: "contacts",
+    tooltip: "Contacts",
+  },
+  { href: "/content/food-park", src: food, alt: "Mess", tooltip: "Mess" },
+  { href: "/content/games", src: sports, alt: "laundary", tooltip: "Laundry" },
+  { href: "/content/night-mess", src: sports, alt: "games", tooltip: "Games" },
+  {
+    href: "/content/study-hub",
+    src: studyHub,
+    alt: "contacts",
+    tooltip: "Study Hub",
+  },
+  {
+    href: "/content/vshare",
+    src: carsharing,
+    alt: "contacts",
+    tooltip: "VShare",
+  },
+  { href: "/content/v-chat", src: chatbot, alt: "vchat", tooltip: "VChat" },
+];
 const Sidebar = () => {
   return (
     <div className=" items-center h-auto w-10 absolute left-0 flex flex-col p-2 gap-6">
-      <Link href="/content/complaints">
-        <TooltipProvider delayDuration={10}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <div>
-                <Image
-                  src={complaints}
-                  height={30}
-                  width={30}
-                  alt="contacts"
-                ></Image>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>Complaints</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </Link>
-      <Link href="/content/contacts">
-        <div>
-          <Image src={calling} height={30} width={30} alt="contacts"></Image>
-        </div>
-      </Link>
-
-      <Link href="/content/food-park">
-        <div>
-          <Image src={food} height={30} width={30} alt="Mess"></Image>
-        </div>
-      </Link>
-      <Link href="/content/games">
-        <div>
-          <Image src={sports} height={30} width={30} alt="laundary"></Image>
-        </div>
-      </Link>
-      <Link href="/content/night-mess">
-        <div>
-          <Image src={sports} height={30} width={30} alt="games"></Image>
-        </div>
-      </Link>
-      <Link href="/content/study-hub">
-        <div>
-          <Image src={studyHub} height={30} width={30} alt="contacts"></Image>
-        </div>
-      </Link>
-      <Link href="/content/vshare">
-        <div>
-          <Image src={carsharing} height={30} width={30} alt="contacts"></Image>
-        </div>
-      </Link>
-      <Link href="/content/v-chat">
-        <div>
-          <Image src={chatbot} height={30} width={30} alt="vchat"></Image>
-        </div>
-      </Link>
+      <TooltipProvider delayDuration={10}>
+        {links.map(({ href, src, alt, tooltip }, index) => (
+          <Link key={index} href={href}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <div>
+                  <Image src={src} height={30} width={30} alt={alt} />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{tooltip}</p>
+              </TooltipContent>
+            </Tooltip>
+          </Link>
+        ))}
+      </TooltipProvider>
     </div>
   );
 };
