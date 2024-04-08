b04cc3af0e406e34ed6326c53a20894d94438cdc
mohdshahzil <theshahzil@gmail.com>
2024-04-08 09:45:00 +0530

    foodpark ui done

---
+++ app/content/food-park/FoodNonVegData.ts
@@ -0,0 +1,6 @@
+import paneerbuttermasaladosa from "@/assets/night-mess/veg/paneer-butter-masala-dosa.png"
+export const foodNonVegData = [
+    { title: "Paneer Masala Dosa", price: "Rs. 60000/-", image: paneerbuttermasaladosa },
+    // { title: "Lassi", price: "Rs. 50/-", image: lassi}
+    // Add more items here
+  ];
