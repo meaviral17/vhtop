6d8c869bc91d871b3cc2f6a0a3fdcb73c17a6a50
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-04-08 13:18:07 +0530

    LostAndFound

--- app/content/complaints/page.tsx
+++ app/content/complaints/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import AppbarLogin from "@/components/Appbar/AppbarLogin";
 import Sidebar from "@/components/Sidebar/Sidebar";
@@ -30,10 +31,22 @@ const page = () => {
 
             <div className="flex flex-col sm:flex-row">
               <div className="w-full sm:w-3/5 border border-stone-300 mb-3 sm:mb-0 py-2 px-3 mt-3 sm:mt-0 ">
-                Create/View Tickets
+                Create Ticket for hostel complaints
               </div>
               <div className="w-full sm:w-3/4 p-3 border border-stone-300">
                 <Link href="/content/complaints/newComplaints">
+                  <Button className="m-2">Create Tickets</Button>
+                </Link>
+
+                <Button>View Tickets</Button>
+              </div>
+            </div>
+            <div className="flex flex-col sm:flex-row">
+              <div className="w-full sm:w-3/5 border border-stone-300 mb-3 sm:mb-0 py-2 px-3 mt-3 sm:mt-0 ">
+                Create ticket for Found
+              </div>
+              <div className="w-full sm:w-3/4 p-3 border border-stone-300">
+                <Link href="/content/complaints/LostandFound">
                   <Button className="m-2">Create Tickets</Button>
                 </Link>
 
