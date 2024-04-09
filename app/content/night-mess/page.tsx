86cfbed60c1c38b09b0aa13619612210912fc233
mohdshahzil <theshahzil@gmail.com>
2024-04-09 15:44:01 +0530

    pay button corrected

--- app/content/night-mess/page.tsx
+++ app/content/night-mess/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import AppbarLogin from "@/components/Appbar/AppbarLogin";
 import Sidebar from "@/components/Sidebar/Sidebar";
@@ -6,61 +6,9 @@ import FoodItems from "@/components/FoodItems/FoodItems";
 import { nightVegData } from "./NightVegData";
 import { nightNonVegData } from "./NightNonVegData";
 import { nightShakesData } from "./NightShakesData";
+import { Button } from "@/components/ui/button";
 const page = () => {
-  const initializeRazorpay = () => {
-    return new Promise((resolve) => {
-      const script = document.createElement("script");
-      script.src = "https://checkout.razorpay.com/v1/checkout.js";
-      // document.body.appendChild(script);
 
-      script.onload = () => {
-        resolve(true);
-      };
-      script.onerror = () => {
-        resolve(false);
-      };
-
-      document.body.appendChild(script);
-    });
-  };
-  const makePayment = async () => {
-    console.log("here...");
-    const res = await initializeRazorpay();
-
-    if (!res) {
-      alert("Razorpay SDK Failed to load");
-      return;
-    }
-
-    // Make API call to the serverless API
-    const data = await fetch("/api/razorpay", { method: "POST" }).then((t) =>
-      t.json()
-    );
-    console.log(data);
-    var options = {
-      key: process.env.RAZORPAY_KEY, // Enter the Key ID generated from the Dashboard
-      name: "Vellore Institute of technology",
-      currency: data.currency,
-      amount: data.amount,
-      order_id: data.id,
-      description: "Thankyou for your test donation",
-      image: "https://manuarora.in/logo.png",
-      handler: function (response: { razorpay_payment_id: any; razorpay_order_id: any; razorpay_signature: any; }) {
-        // Validate payment at server - using webhooks is a better idea.
-        alert(response.razorpay_payment_id);
-        alert(response.razorpay_order_id);
-        alert(response.razorpay_signature);
-      },
-      prefill: {
-        name: "Manu Arora",
-        email: "manuarorawork@gmail.com",
-        contact: "9999999999",
-      },
-    };
-
-    const paymentObject = new (window as any).Razorpay(options);
-    paymentObject.open();
-  };
 
   return (
     <div>
@@ -78,8 +26,8 @@ const page = () => {
         <FoodItems title="Veg Items" foodItems={nightVegData} />
         <FoodItems title="Non-Veg Items" foodItems={nightNonVegData} />
         <FoodItems title="juice" foodItems={nightShakesData} />
-        <button onClick={()=>makePayment()}>Pay now</button>
       </div>
+      
     </div>
   );
 };
