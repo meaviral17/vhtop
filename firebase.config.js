a80423c08a10146e1bed770d9be7fceaa9ba8125
Kanishk Sharma <121635906+Kanishk-S06@users.noreply.github.com>
2024-07-29 23:20:15 +0530

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAH3ZOC5qNpOIibwsV4vc0gYPqqTiHwOes",
-  authDomain: "vhtop-8f95b.firebaseapp.com",
-  projectId: "vhtop-8f95b",
-  storageBucket: "vhtop-8f95b.appspot.com",
-  messagingSenderId: "167988790706",
-  appId: "1:167988790706:web:7fd374089d367909f390ab",
-  measurementId: "G-613QS9SRRB"
+  // add your Firebase Config files here
 };
 
 // Initialize Firebase
